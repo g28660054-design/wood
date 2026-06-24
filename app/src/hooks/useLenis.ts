21f2ef15ecaa06ee1442ed68588ce3c563eb639c
Gangstervegas <g28660054@gmail.com>
2026-06-24 21:22:24 +0300

    CodeEdit: update app/src/hooks/useLenis.ts

---
+++ app/src/hooks/useLenis.ts
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import Lenis from 'lenis';
+
+export function useLenis() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.1,
+      smoothWheel: true,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+}
